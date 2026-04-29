78c7c8fffae3eb6958a88f7dbe3d70028245c643
Senpai <clevercoder0307@gmail.com>
2026-04-29 18:05:03 +0530

    Navbar done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
     <div>
-      Hello World!
+      {/* Navbar */}
+      <Navbar />
+      {/* Hero Section */}
+      {/* About Section */}
+      {/* Project Section */}
+      {/* Client Testimonial */}
+      {/* Service Section */}
+      {/* Sequence Section */}
+      {/* FAQ Section */}
+      {/* Footer */}
     </div>
   );
 }
