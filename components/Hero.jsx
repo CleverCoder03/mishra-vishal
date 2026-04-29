8419aed57561d14fffeff5f7ff5a6b51cce2186e
Senpai <clevercoder0307@gmail.com>
2026-04-29 23:45:22 +0530

    basic tweak in hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -17,20 +17,20 @@ const Hero = () => {
       </div>
 
       {/* Foreground */}
-      <div className="relative min-h-dvh px-5 md:px-12 xl:px-18 pb-12 text-white flex flex-col justify-end">
+      <div className="relative min-h-dvh px-5 md:px-12 xl:px-18 pb-12 lg:pb-18 text-white flex flex-col justify-end">
         <div className="flex flex-col gap-15">
-          <div>
-            <div className="text-6xl">
+          <div className="flex flex-col lg:flex-row lg:justify-between lg:items-end gap-5">
+            <div className="text-6xl lg:text-8xl xl:text-9xl">
               <h1>Freelance</h1>
               <h1>Web Developer</h1>
             </div>
-            <div className="flex items-center gap-2 mt-4 text-lg md:text-xl">
+            <div className="flex items-center gap-2 text-lg md:text-xl">
               <Image src={globeSVG} alt="Globe" className="invert size-5" />
               Mumbai, India
             </div>
           </div>
           <hr />
-          <div className="flex flex-col gap-5">
+          <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center gap-5">
             <h1 className="text-lg md:text-xl">
               I&apos;ll help you make a lasting impression online.
             </h1>
