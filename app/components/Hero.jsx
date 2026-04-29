f560b3009d8608810ceb8102d659cefa433f7214
Senpai <clevercoder0307@gmail.com>
2026-04-29 18:21:43 +0530

    added Hero Section

---
+++ app/components/Hero.jsx
@@ -0,0 +1,14 @@
+import { AuroraBackground } from "@/components/ui/aurora-background"
+import Image from "next/image"
+
+const Hero = () => {
+  return (
+    <AuroraBackground className="h-dvh w-dvw">
+        <div>
+            Hello World!
+        </div>
+    </AuroraBackground>
+  )
+}
+
+export default Hero
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
       {/* Navbar */}
       <Navbar />
       {/* Hero Section */}
+      <Hero />
       {/* About Section */}
       {/* Project Section */}
       {/* Client Testimonial */}
