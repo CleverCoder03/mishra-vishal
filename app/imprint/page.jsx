1dbdf2b33bd9d031f768dfca496061f118426ce6
Senpai <clevercoder0307@gmail.com>
2026-05-09 15:25:04 +0530

    add:imprint page

--- app/imprint/page.jsx
+++ app/imprint/page.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div >
+      <Navbar />
+      <div className="px-5 py-30 lg:py-40 md:px-12 xl:px-18 border-b border-gray-400 [&_h2]:text-2xl [&_h2]:font-bold [&_p]:text-lg [&_p]:text-gray-600 [&_h2]:mt-10 [&_h2]:mb-4">
+        <h1 className='text-3xl font-bold'>Imprint</h1>
+        <div>
+          <h2>Information</h2>
+          <p>Vishal Mishra</p>
+          <p>Mumbai, India</p>
+        </div>
+
+        <div>
+          <h2>Contact</h2>
+          <p>Telephone: +91 7249084224</p>
+          <p>Email: clevercoder0307@gmail.com</p>
+        </div>
+
+        <div>
+          <h2>Consumer dispute resolution/Universal arbitration body</h2>
+          <p>We are not willing or obliged to participate in dispute resolution proceedings before a consumer arbitration board.</p>
+        </div>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
