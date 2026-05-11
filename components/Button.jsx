40a19a9e25b68df12d51925c0c97d88a4676b6a6
Senpai <clevercoder0307@gmail.com>
2026-05-11 08:56:45 +0530

    fix: Button text color

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({className}) => {
   return (
-    <button className={`w-fit px-5 py-2.5 font-semibold transition-colors cursor-pointer ${className || ''}`}>
+    <button className={`w-fit px-5 py-2.5 font-semibold transition-colors cursor-pointer text-black ${className || ''}`}>
       Start Project
     </button>
   );
